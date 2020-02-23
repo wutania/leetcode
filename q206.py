d4bbc6ff6ec490c7030738f0b51945cff98d1e7c
wutania <wutania@gmail.com>
2020-02-23 11:07:45 +0800

    reverse linked list

---
+++ q206.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curr = head
+        prev = None
+        
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        head = prev
+        return head
